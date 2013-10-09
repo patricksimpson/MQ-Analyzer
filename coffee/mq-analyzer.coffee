09bfcb2c821fac5e3707be503ed518c178350bca
patricksimpson <izerop@gmail.com>
2013-10-09 08:59:52 -0400

    added package, coffee files.

---
+++ coffee/mq-analyzer.coffee
@@ -0,0 +1,25 @@
+(->
+  window.MQA =
+    data: []
+    common:
+      init: ->
+        $.each document.styleSheets, (i, sheet) ->
+          $.get sheet.href, (data) ->
+            re = /\@media(?:[^(]|[^print])*?((?:min|max)-width):\s*([0-9]*.?[0-9]*)(em|px|rem)\)/g
+            while matches = re.exec(data)
+              i = 0
+              while i < matches.length
+                mq = matches[i]
+                type = matches[i + 1]
+                value = matches[i + 2]
+                unit = matches[i + 3]
+                MQA.data.push
+                  mq: matches[i]
+                  type: matches[i + 1]
+                  value: matches[i + 2]
+                  unti: matches[i + 3]
+                i = i + 3
+                i++
+  $(document).ready ->
+    MQA.common.init()
+).call this
--- js/mq-analyzer.js
+++ js/mq-analyzer.js
@@ -1,3 +1,34 @@
-$.each(document.styleSheets, function(i, sheet) {
-	console.log(sheet.href);
-});
+(function() {
+  window.MQA = { 
+    data: [],
+    common: {
+      init: function() {
+        $.each(document.styleSheets, function(i, sheet) {
+          console.log(sheet.href);
+          $.get(sheet.href, function(data) {
+            var re, matches;
+            re = /\@media(?:[^(]|[^print])*?((?:min|max)-width):\s*([0-9]*.?[0-9]*)(em|px|rem)\)/gi
+            while(matches = re.exec(data)){
+              for(var i = 0; i < matches.length; i++) {
+                mq = matches[i];
+                type = matches[i+1];
+                value = matches[i+2];
+                unit = matches[i+3];
+                MQA.data.push({
+                  mq: matches[i],
+                  type: matches[i+1],
+                  value: matches[i+2],
+                  unti: matches[i+3]
+                });
+                i = i + 3;
+              }
+            }
+          });
+        });
+      }
+    }
+  };
+  $(document).ready(function(){
+    return MQA.common.init();
+  });
+}).call(this);
